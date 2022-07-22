71a2589d6dcf9bdf2a0c7fd0668bea9ad62dca0e
Petrus Pierre <petrus.tsx@gmail.com>
2022-07-22 11:10:14 -0300

    add sass and typescript

---
+++ pages/contato.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Contact.module.scss'
+
+export default function Contato() {
+  return (
+    <div className={styles.Container}>
+      <h1>Contato</h1>
+    </div>
+  )
+}
