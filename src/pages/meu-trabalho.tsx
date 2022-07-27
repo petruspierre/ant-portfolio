5fbc0813fcf5296326bd8f8f4013b37f4fc9aa50
Petrus Pierre <petrus.tsx@gmail.com>
2022-07-27 20:28:28 -0300

    styling mywork page

--- src/pages/meu-trabalho.tsx
+++ src/pages/meu-trabalho.tsx
@@ -10,10 +10,10 @@ export default function SobreMim() {
         <section className={styles.Hero}>
           <div>
             <div>
-              <p>- Só para ficar claro, o que você faz?</p>
+              <h2>- Só para ficar claro, o que você faz?</h2>
               <h1>Design de interfaces para sites e aplicativos</h1>
 
-              <h2>Eu faço as suas ideias de sites e aplicações virarem realidade.<br/ >Preparo os designs das suas ideias, e depois é só programar.</h2>
+              <p>Eu faço as suas ideias de sites e aplicações virarem realidade.<br/ >Preparo os designs das suas ideias, e depois é só programar.</p>
             </div>
             <div>
               <ArrowLink link="/" label="voltar" right />
@@ -21,10 +21,12 @@ export default function SobreMim() {
           </div>
         </section>
 
-        <footer>
+        <footer className={styles.Footer}>
           <p>Gostou do que viu?</p>
 
           <p>Também estou apto a aprender e desenvolver novos tipos de trabalho. Tudo pode ser conversado, desde que se mantenha em minha área. Quer me contratar? Entre em contato:</p>
+
+          <ArrowLink link="/contato" label="contato" right />
         </footer>
       </div>
     </Layout>
